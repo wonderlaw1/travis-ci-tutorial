09c614c8e41c02565cb4c29a7147c7fa7b1048d6
Kamelevskyi <wonderlaw12@gmail.com>
2020-12-01 17:05:44 +0200

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'travis-ci-tutorial'`, () => {
+  it(`should have as title 'travis-tutorial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('travis-ci-tutorial');
+    expect(app.title).toEqual('travis-tutorial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('travis-ci-tutorial app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('travis-tutorial app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+
+const routes: Routes = [
+  {
+    path: 'page1',
+    component: Cmp1Component,
+  },
+  {
+    path: 'page2',
+    component: Cmp2Component,
+  },
+  {
+    path: 'page3',
+    component: Cmp3Component,
+  },
+  {
+    path: '',
+    redirectTo: 'page1',
+    pathMatch: 'full',
+  }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]

9d91472412ee2b530b7d004515aed23b5e8a52b9
Kamelevskyi <wonderlaw12@gmail.com>
2020-12-01 17:15:28 +0200

    init

---
+++ src/app/cmp2/cmp2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmp2',
+  templateUrl: './cmp2.component.html',
+  styleUrls: ['./cmp2.component.css']
+})
+export class Cmp2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/cmp3/cmp3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmp3',
+  templateUrl: './cmp3.component.html',
+  styleUrls: ['./cmp3.component.css']
+})
+export class Cmp3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
